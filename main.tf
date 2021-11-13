7f1683be517a84d5398fd780f02648c38da27f8a
joaopi <joaopi@users.noreply.github.com>
2021-11-13 16:49:19 +0000

    Add security group to allow traffic
    Add security group to allow traffic (ssh and http/https)

--- main.tf
+++ main.tf
@@ -3,6 +3,33 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_security_group" "allow_traffic" {
+  name        = "Allow http and ssh traffic"
+  description = "Allow ssh and standard http/https ports inbound and everything outbound"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
