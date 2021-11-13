a928cc8bef73bebdd10d1ee8f59a0bc227814446
joaopi <joaopi@users.noreply.github.com>
2021-11-13 15:54:17 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  profile = "default"
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+    "Terraform" = "true"
+  }
+}
