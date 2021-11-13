657b3f946e01952dc7bb85bf3b57474b5bfc89e3
joaopi <joaopi@users.noreply.github.com>
2021-11-13 16:29:41 +0000

    Add jenkins and java installation steps

--- main.tf
+++ main.tf
@@ -23,6 +23,30 @@ resource "aws_instance" "jenkins" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
+  provisioner "remote-exec" {
+    inline = [
+      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update -qq",
+      "sudo apt install -y openjdk-11-jdk",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
+      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
+      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
+      "sudo apt-get -y install iptables-persistent",
+      "sudo ufw allow 8080",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = var.ssh_user_name
+    private_key = file(var.ssh_private_key_file)
+  }
+  
   tags = {
     Name = var.instance_name
     "Terraform" = "true"
