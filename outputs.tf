4fa3135cf489351f46500182142b39dd450c5cf0
joaopi <joaopi@users.noreply.github.com>
2021-11-14 14:55:20 +0000

    Fix key pair output vars

--- outputs.tf
+++ outputs.tf
@@ -10,15 +10,15 @@ output "instance_arn" {
 
 output "key_pair_key_name" {
   description = "The key pair name."
-  value       = aws_key_pair.generated_key.key_pair_key_name
+  value       = aws_key_pair.generated_key[count.index].key_name
 }
 
 output "key_pair_key_pair_id" {
   description = "The key pair ID."
-  value       = aws_key_pair.generated_key.key_pair_key_pair_id
+  value       = aws_key_pair.generated_key[count.index].pair_id
 }
 
 output "key_pair_fingerprint" {
   description = "The MD5 public key fingerprint."
-  value       = aws_key_pair.generated_key.key_pair_fingerprint
+  value       = aws_key_pair.generated_key[count.index].key_pair_fingerprint
 }
